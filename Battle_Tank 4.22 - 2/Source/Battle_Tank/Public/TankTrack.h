8005d9f935aa955105b30e7d434537f6a6af53e9
Bob Sabiston <bob@flatblackfilms.com>
2019-06-26 16:56:51 -0500

    Tank treads

---
+++ Battle_Tank 4.22 - 2/Source/Battle_Tank/Public/TankTrack.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/StaticMeshComponent.h"
+#include "TankTrack.generated.h"
+
+/**
+ *  TankTrack is used to set maximum driving force, and to apply forces to the tank.
+ */
+UCLASS(meta = (BlueprintSpawnableComponent), hidecategories = ("Collision"))
+class BATTLE_TANK_API UTankTrack : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+	
+public:
+	/* Booo dee boop */
+	UFUNCTION(BlueprintCallable, Category = Input)
+	void SetThrottle(float Throttle);
+
+	// Max force per track, in Newtons
+	UPROPERTY(EditDefaultsOnly)
+		float TrackMaxDrivingForce = 400000;  // assume 40 tonne tank and 1g acceleration
+};
